dd0add26d8d2ab99fdcae0826baa95ec5aaa0630
Tom Butcher <thomasbutcher15@gmail.com>
2021-02-27 18:40:11 +0000

    Removed unused includes

--- src/gui/mainwindow.cpp
+++ src/gui/mainwindow.cpp
@@ -1,11 +1,6 @@
 #include "mainwindow.h"
 #include "./ui_mainwindow.h"
 
-#include <QHostInfo>
-#include <QNetworkInterface>
-
-
-
 MainWindow::MainWindow(QWidget *parent)
     : QMainWindow(parent)
     , ui(new Ui::MainWindow)

3163537436ca8d82352612268c4dac78311c7ff5
Tom Butcher <thomasbutcher15@gmail.com>
2021-02-27 18:40:30 +0000

    Embedded fonts into project

--- src/main.cpp
+++ src/main.cpp
@@ -1,12 +1,21 @@
 #include "gui/mainwindow.h"
 
 #include <QApplication>
+#include <QFontDatabase>
 
 int main(int argc, char *argv[])
 {
 
     QApplication a(argc, argv);
     a.setAttribute(Qt::AA_UseHighDpiPixmaps);
+
+    QFontDatabase::addApplicationFont(":/fonts/fonts/Roboto-Bold.ttf");
+    QFontDatabase::addApplicationFont(":/fonts/fonts/Roboto-BoldItalic.ttf");
+    QFontDatabase::addApplicationFont(":/fonts/fonts/Roboto-Medium.ttf");
+    QFontDatabase::addApplicationFont(":/fonts/fonts/Roboto-MediumItalic.ttf");
+    QFontDatabase::addApplicationFont(":/fonts/fonts/Roboto-Regular.ttf");
+    QFontDatabase::addApplicationFont(":/fonts/fonts/Roboto-Italic.ttf");
+
     MainWindow w;
     w.show();
     return a.exec();

c13885be2d1cb155b8dfbae3a5562fd5996994a1
Tom Butcher <thomasbutcher15@gmail.com>
2021-03-01 18:45:07 +0000

    Fix env

--- scripts/win/env.sh
+++ scripts/win/env.sh
@@ -3,10 +3,6 @@ set -exu
 export PATH=$QTDIR/bin:$PATH
 
 export BUILD_PATH=$APPVEYOR_BUILD_FOLDER/build
-export DEPENDENCY_BUILD_PATH=$DEPENDENCY_PATH/build
-export DEPENDENCY_PATH=$APPVEYOR_BUILD_FOLDER/dependencies
-export DOWNLOADS_PATH=$APPVEYOR_BUILD_FOLDER/downloads
-
 export CMAKE_PREFIX=$INSTALL_PREFIX/lib/cmake
 export CMAKE_PREFIX_PATH="$CMAKE_PREFIX:$INSTALL_PREFIX/share/ECM/cmake":$QTDIR/lib/cmake
 
@@ -15,5 +11,3 @@ export APP_VERSION
 export APP=tasmomanager-$APP_VERSION
 
 mkdir -p "$INSTALL_PREFIX"
-mkdir -p "$DEPENDENCY_PATH"
-mkdir -p "$DOWNLOADS_PATH"

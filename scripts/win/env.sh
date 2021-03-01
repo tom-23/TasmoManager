22f24ee7aac6db4ee97bffad556045b51df4c866
Tom Butcher <thomasbutcher15@gmail.com>
2021-03-01 18:48:24 +0000

    Fix 3

--- scripts/win/env.sh
+++ scripts/win/env.sh
@@ -3,11 +3,9 @@ set -exu
 export PATH=$QTDIR/bin:$PATH
 
 export BUILD_PATH=$APPVEYOR_BUILD_FOLDER/build
-export CMAKE_PREFIX=$INSTALL_PREFIX/lib/cmake
+export CMAKE_PREFIX=/lib/cmake
 export CMAKE_PREFIX_PATH="$CMAKE_PREFIX:$INSTALL_PREFIX/share/ECM/cmake":$QTDIR/lib/cmake
 
 APP_VERSION=$(git describe --tags --always HEAD)
 export APP_VERSION
 export APP=tasmomanager-$APP_VERSION
-
-mkdir -p "$INSTALL_PREFIX"

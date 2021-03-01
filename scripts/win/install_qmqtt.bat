9dc66f890b3fd2663723f5298387f697e69b0a19
Tom Butcher <thomasbutcher15@gmail.com>
2021-03-01 20:05:13 +0000

    Fix 7

--- scripts/win/install_qmqtt.bat
+++ scripts/win/install_qmqtt.bat
@@ -1,4 +1,4 @@
-call "C:\Program Files (x86)\Microsoft Visual Studio\2019\Community\VC\Auxiliary\Build\vcvars64.bat"
+cmd /k "C:\Program Files (x86)\Microsoft Visual Studio\2019\Community\VC\Auxiliary\Build\vcvars64.bat"
 qmake ..
 nmake
 nmake install

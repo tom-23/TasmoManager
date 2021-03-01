35589fc2def70fe152e194289a67743e1cc30db9
Tom Butcher <thomasbutcher15@gmail.com>
2021-03-01 19:07:37 +0000

    Fix 5

--- scripts/win/env.sh
+++ scripts/win/env.sh
@@ -1,8 +1,8 @@
 #!/usr/bin/bash
 set -exu
 
-export CMAKE_GENERATOR=Visual Studio 16 2019
-export CMAKE_GENERATOR_ARCH=x64
+export CMAKE_GENERATOR="Visual Studio 16 2019"
+export CMAKE_GENERATOR_ARCH="x64"
 
 export PATH=$QTDIR/bin:$PATH
 

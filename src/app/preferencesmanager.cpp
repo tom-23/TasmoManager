c450c627bfabfd3cb9053c47ba7ae225afac82e9
Tom Butcher <thomasbutcher15@gmail.com>
2021-03-04 18:37:46 +0000

    Added preferences manager

---
+++ src/app/preferencesmanager.cpp
@@ -0,0 +1,53 @@
+#include "preferencesmanager.h"
+
+PreferencesManager::PreferencesManager(QObject *parent) : QObject(parent)
+{
+    QDir dir(QStandardPaths::writableLocation(QStandardPaths::AppDataLocation));
+    if (!dir.exists())
+        dir.mkpath(".");
+    preferencesJSONLocation = QStandardPaths::writableLocation(QStandardPaths::AppDataLocation) + "/.tasmoManagerPreferences.json";
+    qDebug() << "[Prefs Man] JSON Location:" << preferencesJSONLocation;
+}
+
+void PreferencesManager::loadPreferences() {
+    QFile file(preferencesJSONLocation);
+
+    if (!file.exists()) {
+        savePreferences();
+    }
+
+    if (file.open(QFile::ReadOnly)) {
+        QJsonDocument jsonDoc = QJsonDocument::fromJson(file.readAll());
+        QJsonObject root = jsonDoc.object();
+        backupLocation = root.value("backupLocation").toString();
+        useAutomaticDeviceScanning = root.value("useAutomaticDeviceScannig").toBool();
+        firstScanIP = QHostAddress(root.value("lastScanIP").toString());
+        lastScanIP = QHostAddress(root.value("firstScanIP").toString());
+    } else {
+        qDebug() << "[Prefs Man] Could not load preferences file.";
+    }
+
+}
+
+void PreferencesManager::savePreferences() {
+    QFile file(preferencesJSONLocation);
+    if (file.open(QFile::ReadWrite)) {
+        QJsonDocument jsonDoc;
+
+        QJsonObject root;
+        root.insert("backupLocation", backupLocation);
+        root.insert("useAutomaticDeviceScannig", useAutomaticDeviceScanning);
+        root.insert("firstScanIP", firstScanIP.toString());
+        root.insert("lastScanIP", lastScanIP.toString());
+
+        jsonDoc.setObject(root);
+
+        file.resize(0);
+        file.write(jsonDoc.toJson());
+        qDebug() << "[Prefs Man] Saved preferences file!";
+
+    } else {
+        qDebug() << "[Prefs Man] Could not save server file.";
+        qDebug() << "[Prefs Man] Error: " << file.errorString();
+    }
+}
---
+++ src/app/preferencesmanager.h
@@ -0,0 +1,34 @@
+#ifndef PREFERENCESMANAGER_H
+#define PREFERENCESMANAGER_H
+
+#include <QObject>
+#include <QStandardPaths>
+#include <QDebug>
+#include <QFile>
+#include <QJsonDocument>
+#include <QJsonObject>
+#include <QDir>
+#include <QHostAddress>
+
+
+class PreferencesManager : public QObject
+{
+    Q_OBJECT
+public:
+    PreferencesManager(QObject *parent = nullptr);
+    virtual ~PreferencesManager() {};
+
+    // Preference Values
+    QString backupLocation = "";
+    bool useAutomaticDeviceScanning = true;
+    QHostAddress firstScanIP;
+    QHostAddress lastScanIP;
+
+    void loadPreferences();
+    void savePreferences();
+
+private:
+    QString preferencesJSONLocation;
+};
+
+#endif // PREFERENCESMANAGER_H

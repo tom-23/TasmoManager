c3dccc123ec261b2146abe1025f95d44b7bc1b37
Tom Butcher <thomasbutcher15@gmail.com>
2021-04-13 22:33:31 +0100

    Published linux dist file

--- scripts/linux/after_build.sh
+++ scripts/linux/after_build.sh
@@ -3,6 +3,11 @@ source scripts/macOS/env.sh
 
 cd build
 cqtdeployer -bin tasmomanager -extraLibs qmqtt -targetDir ./Distro
+cd Distro
+tar -czvf "../linux-${APP}-dist.tar.gz" *
+cd ..
+appveyor PushArtifact "linux-${APP}-dist.tar.gz" -DeploymentName "TasmoManager Linux Distribution"
+
 cpack .
 cd packages
 for f in *.deb; do sudo dpkg -i "$f"; done
